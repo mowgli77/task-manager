68481ff74dd72f0f3ab3f10d5010831831a01847
mowgli77 <bartend.it13@gmail.com>
2020-10-16 13:51:18 +0300

    the second commit of task-manager

---
+++ client/build/precache-manifest.478cd9cb89969b8bf9e9d12231af17f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c32bffa9a4bdeffa566cd9487b6b2b0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "951833db359f63f74b66",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "85277d31d24b6fc94f21",
+    "url": "/static/js/2.46a2d280.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.46a2d280.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "951833db359f63f74b66",
+    "url": "/static/js/main.c474ca65.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
